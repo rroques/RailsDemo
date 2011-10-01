6be2d54e216d8911c88d5b66c1985437829ce513
Remi Roques <remi.roques@gmail.com>
2011-10-01 15:20:17 +1300

    User mgmt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,43 @@ class UsersController < ApplicationController
 
   def new
     @title = "Sign up"
+    @user = User.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @user }
+    end
+
+  end
+  
+  def index 
+    @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+    @Title = @user.name
   end
+  
+  def edit
+    @user = User.find(params[:id])
+    @Title = @user.name
+  end
+  
+  # PUT /tracks/1
+  # PUT /tracks/1.xml
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:track])
+        format.html { redirect_to(@user, :notice => 'User was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,12 @@
 #  created_at :datetime
 #  updated_at :datetime
 #
-
+require 'digest'
 class User < ActiveRecord::Base
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  attr_accessible :firstname, :lastname, :email
+  attr_accessor :password
+  attr_accessible :firstname, :lastname, :email, :password, :password_confirmation
   validates :firstname, :presence => true,
                     :length   => { :maximum => 50 }
   validates :lastname, :presence => true,
@@ -23,4 +23,46 @@ class User < ActiveRecord::Base
                       :format => {:with => email_regex},
                     :uniqueness => { :case_sensitive => false }
 
+  validates :password, :presence     => true,
+                       :confirmation => true,
+                       :length       => { :within => 6..40 }
+                       
+  before_save :encrypt_password
+  
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+  
+  def name
+    self.firstname + ' ' + self.lastname
+  end
+  
+  private
+
+    def encrypt_password
+      self.salt = make_salt unless has_password?(password)
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      self.encrypted_password = encrypt(password)
+    end
+    
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
